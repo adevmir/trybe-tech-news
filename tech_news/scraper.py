4425336983e38716423b9b970c1c0408ad36ca5c
adevmir <ademirvalonqar@gmail.com>
2023-03-14 16:29:00 -0300

    [Requisito 01] - Implementa funcao fetch que recebe uma url e retorna uma requisicao get da mesma em formato de texto

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,21 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui :"""
+    try:
+        time.sleep(1)
+        response = requests.get(
+            url,
+            headers={"user-agent": "Fake user-agent"},
+            timeout=3
+        )
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.exceptions.Timeout:
+        return None
 
 
 # Requisito 2
