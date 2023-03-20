42006834d637d28ca202e9cae336634a763b356c
adevmir <ademirvalonqar@gmail.com>
2023-03-20 20:43:04 -0300

    [Requisito 8] - implementa funcao search_by_date que recebe uma data e retorna a lista de noticias encontradas com a data compativel, Caso formato seja indesejado retorna Data Invalida

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 
 # Requisito 7
@@ -13,7 +14,17 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    print(date)
+    try:
+        bd_date = datetime.datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        print(bd_date)
+        news_list = search_news({"timestamp": bd_date})
+        result = []
+        for news in news_list:
+            result.append((news["title"], news["url"]))
+        return result
+    except ValueError:
+         raise ValueError("Data inválida")
 
 
 # Requisito 9
